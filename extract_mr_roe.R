b6c5b7698e36c158eaf5299a61d0183f81a546e2
Aditya Mittal <39227064+adityamittal13@users.noreply.github.com>
2021-09-22 07:04:44 -0700

    Update extract_mr_roe.R

--- extract_mr_roe.R
+++ extract_mr_roe.R
@@ -3,7 +3,7 @@ exposures = c('ebi-a-GCST004627', 'ebi-a-GCST004629', 'ebi-a-GCST004608', 'ebi-a
               'ieu-a-1056 ', 'ieu-a-271', 'ukb-b-1937', 'prot-a-266', 'ukb-d-30020_irnt')
 outcomes = c('ebi-a-GCST005038', 'ukb-b-18335', 'ukb-b-12930', 'ukb-b-11497', 'ukb-b-7872', 'ukb-b-14944')
 
-df = NULL
+df = NULL 
 for (exposure in exposures){
   for (outcome in outcomes){
     if (file.exists(paste0("RES_MOE/", exposure, "_", outcome, ".csv"))){
